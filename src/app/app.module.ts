2246ce617a9abfc5bcbe48b17291ec267e23e31e
novikovprog <novikovprog@gmail.com>
2021-12-26 17:58:36 +0100

    add products module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ProductsModule } from './products/products.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProductsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
